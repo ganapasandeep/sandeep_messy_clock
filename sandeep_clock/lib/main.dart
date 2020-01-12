6e817eaec7f7d3c3426358c0371df0b800d1ff0f
Sandeep Ganapa <ganapasandeep@gmail.com>
2020-01-12 15:30:32 +0100

    final commit

--- sandeep_clock/lib/main.dart
+++ sandeep_clock/lib/main.dart
@@ -3,13 +3,11 @@
 // found in the LICENSE file.
 
 import 'dart:io';
-
+import 'package:analog_clock/myfinaleditedclock.dart';
 import 'package:flutter_clock_helper/customizer.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import 'analog_clock.dart';
 
 void main() {
   // A temporary measure until Platform supports web and TargetPlatform supports
